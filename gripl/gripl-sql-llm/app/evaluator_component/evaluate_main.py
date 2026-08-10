a20627c668e984680fc4db362e1068e6420b3bdf
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-08-10 17:16:16 +0200

    adpat pipeline with all needed component

--- gripl/gripl-sql-llm/app/evaluator_component/evaluate_main.py
+++ gripl/gripl-sql-llm/app/evaluator_component/evaluate_main.py
@@ -1,9 +1,74 @@
-from . evaluator import Evaluator
+import os
+
+from .evaluator import Evaluator
 from app.pipeline_component.pipeline import PipelineComponent
 from app.data_loader_component.data_loader import DataLoader
+from app.chroma_database_client.chroma_db_client import ChromaDatabaseClient
+from app.find_intention_component.find_intention import FindIntention
+from app.sql_execution_component.sql_execution import SQLExecution
+from app.sql_generation_component.sql_generation import SQLGenerator
+from app.sql_post_processing_component.sql_post_processing import SQLPostProcessing
+from sentence_transformers import SentenceTransformer
+from app.llm_component.llm import LLM
+from app.prompt_management_component.prompt_management import PromptManagement
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-pipeline = PipelineComponent()
+prompt_management = PromptManagement()
+
+intention_llm = LLM(
+    model_name="openai/gpt-oss-120b",
+    model_url="",
+    api_key=os.getenv("OPENAI_API_KEY"),
+    schema_output=None
+)
+
+sql_generator_llm = LLM(
+    model_name="openai/gpt-oss-120b",
+    model_url="",
+    api_key=os.getenv("OPENAI_API_KEY"),
+    schema_output=None
+)
+
+sql_post_processing_llm = LLM(
+    model_name="openai/gpt-oss-120b",
+    model_url="",
+    api_key=os.getenv("OPENAI_API_KEY"),
+    schema_output=None
+)
+
+model = SentenceTransformer('intfloat/multilingual-e5-small')
+
+chroma_db_client = ChromaDatabaseClient(
+    model
+)
+
+intention_component = FindIntention(
+    llm=intention_llm,
+    prompt_management=prompt_management,
+)
+
+sql_generator = SQLGenerator(
+    llm=sql_generator_llm,
+    prompt_management=prompt_management,
+)
+
+sql_post_processing = SQLPostProcessing(
+    llm=sql_post_processing_llm,
+    prompt_management=prompt_management,
+)
+
+sql_execution = SQLExecution()
+
+pipeline = PipelineComponent(
+    chroma_db_client=chroma_db_client,
+    find_intention=intention_component,
+    sql_execution=sql_execution,
+    sql_generation=sql_generator,
+    post_processing=sql_post_processing,
+)
 
 data_loader = DataLoader()
 
--- gripl/gripl-sql-llm/app/pipeline_component/pipeline.py
+++ gripl/gripl-sql-llm/app/pipeline_component/pipeline.py
@@ -1,7 +1,21 @@
+from app.chroma_database_client.chroma_db_client import ChromaDatabaseClient
+from app.find_intention_component.find_intention import FindIntention
+from app.sql_execution_component.sql_execution import SQLExecution
+from app.sql_generation_component.sql_generation import SQLGenerator
+from app.sql_post_processing_component.sql_post_processing import SQLPostProcessing
+
+
 class PipelineComponent:
 
     def __init__(self,
-
-
+            chroma_db_client: ChromaDatabaseClient,
+            find_intention: FindIntention,
+            sql_execution: SQLExecution,
+            sql_generation: SQLGenerator,
+            post_processing: SQLPostProcessing
                  ):
-        pass
+        self.chroma_db_client = chroma_db_client
+        self.find_intention = find_intention
+        self.sql_execution = sql_execution
+        self.sql_generation = sql_generation
+        self.post_processing = post_processing
