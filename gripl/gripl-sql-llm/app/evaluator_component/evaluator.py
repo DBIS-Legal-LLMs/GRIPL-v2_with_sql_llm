1a9b6db30ab88e5d0fec19ffe423afefab2a5f45
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-08-13 11:49:26 +0200

    save state

--- gripl/gripl-sql-llm/app/evaluator_component/evaluator.py
+++ gripl/gripl-sql-llm/app/evaluator_component/evaluator.py
@@ -1,8 +1,6 @@
-from logging import critical
-
 from app.data_loader_component.data_loader import DataLoader
 from app.pipeline_component.pipeline import PipelineComponent
-import re
+
 
 class Evaluator:
 
@@ -29,7 +27,10 @@ class Evaluator:
                     possible_critical_element = self.pipe_line.get_sid_and_reason_if_critical(activity)
                     if possible_critical_element:
                         critical_elements.append(possible_critical_element)
-                
+                print("critival elments ")
+                print(critical_elements)
+                break
+
             return critical_elements
 
         except Exception as e:
