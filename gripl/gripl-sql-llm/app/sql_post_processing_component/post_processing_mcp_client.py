9ddce78541287323e4d5ad55a709e3dc1ca20ad5
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-09-14 11:32:18 +0200

    improve eror in system prompt

--- gripl/gripl-sql-llm/app/sql_post_processing_component/post_processing_mcp_client.py
+++ gripl/gripl-sql-llm/app/sql_post_processing_component/post_processing_mcp_client.py
@@ -59,6 +59,8 @@ class PostProcessingMcpClient:
                     verification_system_prompt = sql_post_processing_component.prompt_management.fill_prompt(
                         sql_post_processing_component.verification_system_prompt_path)
 
+                    print("system", verification_system_prompt)
+
                     verification_component_messages = [
                         {
                             "role": "system",
